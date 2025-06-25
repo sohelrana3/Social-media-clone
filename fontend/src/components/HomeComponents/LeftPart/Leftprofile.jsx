b699ba562f2925e968f8afccc8a0a0d9899724cf
Sohel Rana <89797602+sohelrana3@users.noreply.github.com>
2025-06-25 21:33:45 +0600

    Update Leftprofile.jsx

--- fontend/src/components/HomeComponents/LeftPart/Leftprofile.jsx
+++ fontend/src/components/HomeComponents/LeftPart/Leftprofile.jsx
@@ -5,7 +5,7 @@ const Leftprofile = () => {
     <>
       <div className="w-28 h-28 rounded-full bg-cyan-100 mx-auto"></div>
       <div className="text-center mt-4">
-        <h1 className="text-lg font-bold capitalize">One It Park </h1>
+        <h1 className="text-lg font-bold capitalize text-black">One It Park </h1>
         <h4 className="font-normal text-base text-gray-500">oneitpark123@gmail.com</h4>
       </div>
     </>
